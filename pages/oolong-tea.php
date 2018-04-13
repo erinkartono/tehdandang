c125c592c56ef4725c2b9ad379f539de369ba8f1
erinkartono <erin.kartono@gmail.com>
2018-04-13 15:23:58 +0700

    Added source code for tehdandang website

---
+++ pages/oolong-tea.php
@@ -0,0 +1,9 @@
+    <section id="blog" class="container">
+        <div class="center">
+            <br><br><br>
+            <h2>Oolong Tea</h2>
+            <p class="lead"><i>lowers bas cholesterol levels and aids with weight loss</i></p>
+            <center><img class="img-responsive img-blog" src="images/dandang/oolongtea.png" width="50%" alt="" /></center>
+            <p class="lead" align="justify"><font color="black">Nama Teh Oolong baru-baru ini mulai dikenal di masyarakat. Secara fisik, penampilan kering dari teh oolong ini berupa bulatan kecil saja. Namun ketika diseduh, bulatan kecil tersebut akan mengembang menjadi beberapa helai daun. Teh ini berbeda dari teh hijau yang polifenolnya masih utuh maupun teh hitam yang polifenolnya sudah berubah menjadi zat lain. Teh oolong berada diantara kedua teh hijau dan teh hitam, sehingga mempunyai kandungan antioksidan yang lebih lengkap.<br><br>Seduhlah beberapa butir teh Oolong cap <strong>DANDANG</strong> menjadi secangkir teh dan temukan keunikan aromanya, rasanya yang lembut berkesan manis dan warna keemasan yang cantik.</font></p>
+        </div>
+    </section><!--/#blog-->
---
+++ pages/white-tea.php
@@ -0,0 +1,9 @@
+    <section id="blog" class="container">
+        <div class="center">
+            <br><br><br>
+            <h2>White Tea</h2>
+            <p class="lead"><i>aids weightloss, antioxidants, healthier skin</i></p>
+            <center><img class="img-responsive img-blog" src="images/dandang/whitetea.png" width="50%" alt="" /></center>
+            <p class="lead" align="justify"><font color="black">Teh Putih atau White Tea dibuat dari bagian pucuk teh yang masih kuncup (dikenal sebagai peko). Disebut White Tea karena penampakannya berwarna putih keperakan mengkilat yang berasal dari bulu-bulu yang menyelimutinya. Peko memiliki kandungan antioksidan tertinggi yang tetap terjaga hingga menjadi white tea. Jika Anda meminum white tea yang diseduh selama 3 menit, maka Anda akan mendapatkan rasa white tea yang lembut, menyegarkan serta beraroma wangi.</font></p>
+        </div>
+    </section><!--/#blog-->
