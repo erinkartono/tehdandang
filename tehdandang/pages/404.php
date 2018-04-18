f9bf1020921e4d8c81a47b0e8e5c0fb102fefb03
erinkartono <erin.kartono@gmail.com>
2018-04-18 13:14:40 +0700

    changed info title to career

--- tehdandang/pages/404.php
+++
@@ -1,6 +0,0 @@
-    <section id="error" class="container text-center">
-    	<br><br><br>
-        <h1>404, Page not found</h1>
-        <p>The Page you are looking for doesn't exist or an other error occurred.</p>
-        <a class="btn btn-primary" href="index.php">GO BACK TO THE HOMEPAGE</a>
-    </section><!--/#error-->
--- tehdandang/pages/green-tea.php
+++
@@ -1,9 +0,0 @@
-    <section id="blog" class="container">
-        <div class="center">
-            <br><br><br>
-            <h2>White Tea</h2>
-            <p class="lead"><i>boosts metabolism, contains antioxidants, energy boost</i></p>
-            <center><img class="img-responsive img-blog" src="images/dandang/greentea.png" width="50%" alt="" /></center>
-            <p class="lead" align="justify"><font color="black">Jenis teh yang satu ini umum dikenal sebagai teh kesehatan, terutama dalam melangsingkan tubuh. Selain itu, sebagian masyarakat juga percaya jika dengan mengkonsumsi teh hijau dapat menurunkan tekanan darah tinggi. Polifenol yang dijaga tetap utuh selama proses pengolahan, menciptakan citarasa sepat yang kuat dan warna air seduah kuning kehijauan. Di Indonesia, teh hijau ini umumnya diolah lebih lanjut menjadi teh wangi melati.</font></p>
-        </div>
-    </section><!--/#blog-->
